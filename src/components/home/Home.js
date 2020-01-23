6752c0dfd33bf5b5858365b3c4424d437446a337
ALEJ4NDRO29 <josealejandro.r.29@gmail.com>
2020-01-23 18:23:36 +0100

    home redux

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+
+const mapDispatchToProps = dispatch => {
+    // dispatch({type: 'test', payload: 'test'});
+}
 
 class Home extends Component {
+    constructor(props) {
+        super(props);
+    }
+    
 
     render() {
-        return(
+
+        return (
             <div>
                 <h1>Listado de hoteles:</h1>
-                // Cargar listado aquí
+            // Cargar listado aquí
             </div>
-        )
+        );
     }
 }
 
-export default Home;
+export default connect(mapDispatchToProps)(Home);
