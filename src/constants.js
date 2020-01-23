9902f477f68eb7b13a4a82c215ba681f7f5716a0
Juan Antonio <juagisla@gmail.com>
2020-01-23 18:21:10 +0100

    Starting home react-redux

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,9 @@
 export const API_URL = 'http://localhost:8000';
+
+export const HOME_PAGE_LOADED = 'HOME_PAGE_LOADED';
+export const HOME_PAGE_UNLOADED = 'HOME_PAGE_UNLOADED';
+export const ASYNC_START = 'ASYNC_START';
+export const ASYNC_END = 'ASYNC_END';
+export const LOGIN = 'LOGIN';
+export const LOGOUT = 'LOGOUT';
+export const REGISTER = 'REGISTER';
