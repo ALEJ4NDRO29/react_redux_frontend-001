0c0dc255aa684cadc1dee963947034fbe6d5c8a3
ALEJ4NDRO29 <josealejandro.r.29@gmail.com>
2020-01-31 17:18:18 +0100

    register form

--- src/components/register/Register.js
+++ src/components/register/Register.js
@@ -1,25 +1,81 @@
 import React, { Component } from "react";
 import { Form, Button } from "react-bootstrap";
+import { connect } from "react-redux";
+
+
+const mapStateToProps = state => ({ ...state.auth });
+
+const mapDispatchToProps = dispatch => ({
+
+})
 
 class Register extends Component {
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            username: '',
+            email: '',
+            password: '',
+            password2: ''
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    handleChange(e) {
+        this.setState({ [e.target.name]: e.target.value });
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+        if (this.state.password && this.state.password === this.state.password2) {
+            console.log('OKOKOKOK');
+        } else {
+            console.log('INVALIDPASS');
+        }
+    }
+
     render() {
         return (
             <div>
                 <h1>Register</h1>
-                <Form>
+                <Form onSubmit={this.onSubmit}>
                     <Form.Group>
                         <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Control
+                            onChange={this.handleChange}
+                            name="username"
+                            type="text"
+                            placeholder="Enter username" />
+                    </Form.Group>
+
+                    <Form.Group>
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control 
+                            onChange={this.handleChange} 
+                            name="email" 
+                            type="email" 
+                            placeholder="Enter email" />
                     </Form.Group>
 
                     <Form.Group>
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
+                        <Form.Control
+                            onChange={this.handleChange}
+                            name="password" 
+                            type="password" 
+                            placeholder="Password" />
                     </Form.Group>
 
                     <Form.Group>
                         <Form.Label>Repeat password</Form.Label>
-                        <Form.Control type="password" placeholder="Repeat password" />
+                        <Form.Control 
+                            onChange={this.handleChange} 
+                            name="password2" 
+                            type="password" 
+                            placeholder="Repeat password" />
                     </Form.Group>
                     <Button variant="outline-dark" className="btn-block" type="submit">
                         Register
@@ -30,4 +86,4 @@ class Register extends Component {
     }
 }
 
-export default Register;
+export default connect(mapStateToProps, mapDispatchToProps)(Register);
